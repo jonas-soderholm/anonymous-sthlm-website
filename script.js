4e50d297ea915f8bcd8640a942f2215c786ada87
Jonas <jonas.soderholm89@gmail.com>
2023-11-07 09:55:14 +0100

    Finish header/navbar; initialize main section
    - Finalize Header and Navbar.
    - Add hero background in Main.
    - Start letter animation.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Header
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu"); // Change to ".nav-menu"
 
@@ -14,4 +16,26 @@ navLinks.forEach((link) => {
 
     console.log(`Clicked on "${link.textContent}" link`);
   });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const overlay = document.getElementById("overlay");
+  const animatedLetter = document.getElementById("animatedLetter");
+  const letters = "ANONYMOUS".split("");
+  let currentLetterIndex = 0;
+
+  const animateLetter = () => {
+    if (currentLetterIndex < letters.length) {
+      animatedLetter.textContent = letters[currentLetterIndex++];
+      setTimeout(animateLetter, 250); // Continue to the next letter after a delay
+    } else {
+      // Once the animation is complete, fade out the overlay
+      overlay.style.transition = "opacity 1s ease-in-out";
+      overlay.style.opacity = "0";
+      setTimeout(() => (overlay.style.display = "none"), 1000); // Remove the overlay after the fade-out is complete
+    }
+  };
+
+  // Start the animation
+  animateLetter();
 });
