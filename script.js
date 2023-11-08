d894da1a724853f3468d5be55e9ff13477bda508
Jonas <jonas.soderholm89@gmail.com>
2023-11-08 14:13:46 +0100

    Style and center calc modal; adjust list layout

--- script.js
+++ script.js
@@ -74,3 +74,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   animateLetter();
 });
+
+//Prompt calculator
+
+var modal = document.getElementById("myModal");
+var btn = document.querySelector(".calculateButton");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
